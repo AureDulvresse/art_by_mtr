a332a54742168ea4e5ab74810ca739700a17be9b
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-06-25 09:07:55 +0100

    Admin interface updated

--- store/utils.py
+++ store/utils.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from .models import Cart
 
 def get_cart_items(user):
-    cart = get_object_or_404(Cart, customer=user)
+    cart = Cart.objects.get(customer = user)
     orders = cart.orders.all()
     total_cost = cart.total_cost
     return orders, total_cost
