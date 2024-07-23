498a1c3f7f7b59532d0cf9c427e354f2825b4177
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-07-23 09:22:27 +0100

    login page  updated

--- store/templatetags/store_custom_tags.py
+++ store/templatetags/store_custom_tags.py
@@ -1,9 +1,25 @@
 from django import template
+from django.forms.boundfield import BoundField
 import os
 
 from django.conf import settings
 
 register = template.Library()
+
+@register.filter(name='add_class')
+def add_class(field, css_class):
+    if isinstance(field, BoundField):
+        return field.as_widget(attrs={"class": css_class})
+    return field
+
+@register.filter(name='add_class_if_error')
+def add_class_if_error(field, css_class):
+    if isinstance(field, BoundField):
+        if field.errors:
+            css_class += ' is-invalid'
+        return field.as_widget(attrs={"class": css_class})
+    return field
+
 
 @register.inclusion_tag(os.path.join(settings.COMPONENTS_DIRS.get('global'), "breadcrumb.html"))
 def breadcrumb(title, msg, user, cart_items, *args, **kwargs):
