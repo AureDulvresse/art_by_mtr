de0e2bca89d52129858e9d2e08d18458eb92c56b
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-07-24 17:15:28 +0100

    breadcrumb  updated

--- store/views.py
+++ store/views.py
@@ -6,28 +6,23 @@ from django.core.mail import send_mail
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic.base import TemplateView
+
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 
 import json
-import paypalrestsdk
 import stripe
 
 from store.models import Artwork, Cart, CheckOut, Order
 from store.utils import get_cart_items
 from blog.models import Post
-
+from art_by_mtr.settings import PAYPAL_CLIENT_ID
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-
-def home_page(request):
-    artworks = Artwork.objects.all().order_by('-updated_at')[:3]
-    posts = Post.objects.all().order_by('-updated_at')[:3]
-    
+# Fonction utilitaire pour obtenir les items du panier
+def get_cart_items_preview(request):
     cart_items = None
     if request.user.is_authenticated:
         try:
@@ -35,6 +30,13 @@ def home_page(request):
             cart_items = cart.orders.select_related('artwork').order_by('-updated_at')
         except Cart.DoesNotExist:
             cart = None
+    return cart_items
+
+def home_page(request):
+    artworks = Artwork.objects.all().order_by('-updated_at')[:3]
+    posts = Post.objects.all().order_by('-updated_at')[:3]
+    
+    cart_items = get_cart_items_preview(request)
 
     context = {
         'artworks': artworks,
@@ -44,34 +46,17 @@ def home_page(request):
 
     return render(request, 'store/pages/home.html', context)
 
-
 def about_page(request):
-    cart_items = None
-    if request.user.is_authenticated:
-        try:
-            cart = Cart.objects.get(customer=request.user)
-            cart_items = cart.orders.select_related('artwork').order_by('-updated_at')
-        except Cart.DoesNotExist:
-            cart = None
-
+    cart_items = get_cart_items_preview(request)
     context = {
         'preview_cart_items': cart_items[:3] if cart_items else None,
     }
-
     return render(request, "store/pages/about.html", context)
-
 
 def gallery_page(request):
     artworks = Artwork.objects.all().order_by("-updated_at")
 
-    cart_items = None
-    user = request.user
-    if user.is_authenticated:
-        try:
-            cart, _ = get_cart_items(user)
-            cart_items = cart.select_related('artwork').order_by('-updated_at')
-        except Cart.DoesNotExist:
-            cart = None
+    cart_items = get_cart_items_preview(request)
 
     paginator = Paginator(artworks, settings.ELEMENTS_PER_PAGE)
     page = request.GET.get('page')
@@ -84,19 +69,10 @@ def gallery_page(request):
 
     return render(request, 'store/pages/gallery.html', context)
 
-
 def artwork_detail_page(request, slug):
-    cart_items = None
-    user = request.user
-    if user.is_authenticated:
-        try:
-            cart, _ = get_cart_items(user)
-            cart_items = cart.select_related('artwork').order_by('-updated_at')
-        except Cart.DoesNotExist:
-            cart = None
+    cart_items = get_cart_items_preview(request)
 
     artwork = get_object_or_404(Artwork, slug=slug)
-    
     current_category = artwork.category
     related_artworks = Artwork.objects.exclude(id=artwork.id).filter(category=current_category)[:3]
     
@@ -108,17 +84,8 @@ def artwork_detail_page(request, slug):
 
     return render(request, 'store/pages/artwork-detail.html', context)
 
-
 def contact_page(request) -> HttpResponse:
-
-    cart_items = None
-    user = request.user
-    if user.is_authenticated:
-        try:
-            cart, _ = get_cart_items(user)
-            cart_items = cart.select_related('artwork').order_by('-updated_at')
-        except Cart.DoesNotExist:
-            cart = None
+    cart_items = get_cart_items_preview(request)
 
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -126,12 +93,11 @@ def contact_page(request) -> HttpResponse:
         subject = request.POST.get('subject')
         message = request.POST.get('message')
 
-        # Validation simple des données du formulaire
+        # Validation des données du formulaire
         if not name or not email or not subject or not message:
             messages.error(request, "Tous les champs sont requis.")
             return redirect('store:contact')
 
-        # Validation de l'adresse email
         if '@' not in email or '.' not in email:
             messages.error(request, "Veuillez entrer une adresse email valide.")
             return redirect('store:contact')
@@ -166,9 +132,7 @@ def contact_page(request) -> HttpResponse:
         'preview_cart_items': cart_items[:3] if cart_items else None, 
     }
     
-
     return render(request, "store/pages/contact.html", context)
-
 
 @login_required
 def cart_page(request):
@@ -183,15 +147,13 @@ def cart_page(request):
 
     return render(request, "store/pages/cart.html", context)
 
-
-
 @login_required
 def checkout_page(request):
     user = request.user
     cart, total_cost = get_cart_items(user)
 
     if request.method == "POST":
-        CheckOut.objects.create(customer=user, orders=cart.orders.all())
+        CheckOut.objects.create(customer=user, orders=cart)
         cart.delete()
         return redirect("store:shop")
 
@@ -202,7 +164,6 @@ def checkout_page(request):
     }
 
     return render(request, "store/pages/checkout.html", context)
-
 
 @csrf_exempt
 @login_required
@@ -217,7 +178,7 @@ def add_to_cart(request):
         except Artwork.DoesNotExist:
             return JsonResponse({"error": "Artwork not found"}, status=404)
 
-        cart, created = Cart.objects.get_or_create(customer=request.user)
+        cart, _ = Cart.objects.get_or_create(customer=request.user)
         order, created = Order.objects.get_or_create(customer=request.user, artwork=artwork, ordered=False)
 
         if not created:
@@ -238,7 +199,6 @@ def add_to_cart(request):
                              status=200)
     return JsonResponse({"error": "Invalid request"}, status=400)
 
-
 @csrf_exempt
 @login_required
 def remove_from_cart(request):
@@ -246,203 +206,30 @@ def remove_from_cart(request):
         data = json.loads(request.body)
         order_uuid = data.get('order_uuid')
 
-        if request.user.is_authenticated:
-            cart_items = None
-            try:
-                order = get_object_or_404(Order, uuid=order_uuid)
-                cart = get_object_or_404(Cart, customer=request.user)
+        try:
+            order = get_object_or_404(Order, uuid=order_uuid)
+            cart = get_object_or_404(Cart, customer=request.user)
+
+            if order in cart.orders.all():
+                cart.orders.remove(order)
+                order.delete()
+
                 cart_items = cart.orders.select_related('artwork').order_by('-updated_at')
 
-                if order in cart.orders.all():
-                    cart.orders.remove(order)
-                    order.delete()
+                # Récupérer les données mises à jour du panier
+                cart_items_html = render_to_string('store/partials/cart_items.html', {'preview_cart_items': cart_items[:3]})
+                cart_items_table_html = render_to_string('store/partials/cart_items_table.html', {'cart': cart_items})
 
-                    # Récupérer les données mises à jour du panier
-                    cart_items_html = render_to_string('store/partials/cart_items.html', {'preview_cart_items': cart_items[:3]})
-                    cart_items_table_html = render_to_string('store/partials/cart_items_table.html', {'cart': cart_items})
+                return JsonResponse({
+                    'success': True, 
+                    'cart_items_html': cart_items_html, 
+                    'cart_items_table_html': cart_items_table_html,
+                })
+            else:
+                return JsonResponse({'success': False, 'error': 'Order not in cart'}, status=400)
+        except Exception as e:
+            return JsonResponse({'success': False, 'error': 'Invalid request'}, status=400)
 
-                    return JsonResponse({
-                        'success': True, 
-                        'cart_items_html': cart_items_html, 
-                        'cart_items_table_html': cart_items_table_html,
-                    })
-                
-                else:
-                    return JsonResponse({'success': False, 'error': 'Order not in cart'}, status=400)
-                    
-            except Exception as e:
-                return JsonResponse({'success': False, 'error': 'Invalid request'}, status=400)
+    return JsonResponse({'success': False, 'error': 'User not authenticated'}, status=401)
 
-        return JsonResponse({'success': False, 'error': 'User not authenticated'}, status=401)
-
-
-# @method_decorator(login_required, name='dispatch')
-# class StripePaymentView(TemplateView):
-#     template_name = 'payments/stripe_payment.html'
-
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['stripe_public_key'] = settings.STRIPE_PUBLIC_KEY
-#         return context
-
-# @login_required
-# def create_checkout_session(request):
-#     if request.method == 'POST':
-#         domain_url = 'http://localhost:8000/'  # Remplacez par votre domaine
-#         checkout = CheckOut.objects.create(customer=request.user)
-#         orders = request.user.orders.filter(ordered=False)
-#         for order in orders:
-#             checkout.orders.add(order)
-
-#         line_items = []
-#         for order in orders:
-#             line_items.append({
-#                 'price_data': {
-#                     'currency': 'usd',
-#                     'artwork_data': {
-#                         'name': order.artwork.title,
-#                     },
-#                     'unit_amount': int(order.artwork.price * 100),  # Convert to cents
-#                 },
-#                 'quantity': order.quantity,
-#             })
-
-#         try:
-#             checkout_session = stripe.checkout.Session.create(
-#                 payment_method_types=['card'],
-#                 line_items=line_items,
-#                 mode='payment',
-#                 success_url=domain_url + 'payments/success/',
-#                 cancel_url=domain_url + 'payments/cancel/',
-#             )
-            
-#             # Enregistrer les détails du paiement dans la base de données
-#             Payment.objects.create(
-#                 user=request.user,
-#                 stripe_checkout_id=checkout_session.id,
-#                 amount=sum(order.artwork.price * order.quantity for order in orders),
-#                 currency='usd',
-#                 status='created',
-#                 checkout=checkout
-#             )
-
-#             return JsonResponse({'id': checkout_session.id})
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=400)
-
-# @csrf_exempt
-# def stripe_webhook(request):
-#     payload = request.body
-#     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-#     endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
-
-#     try:
-#         event = stripe.Webhook.construct_event(
-#             payload, sig_header, endpoint_secret
-#         )
-#     except ValueError as e:
-#         # Invalid payload
-#         return HttpResponse(status=400)
-#     except stripe.error.SignatureVerificationError as e:
-#         # Invalid signature
-#         return HttpResponse(status=400)
-
-#     # Handle the checkout.session.completed event
-#     if event['type'] == 'checkout.session.completed':
-#         session = event['data']['object']
-
-#         # Mettre à jour l'état du paiement
-#         payment = Payment.objects.get(stripe_checkout_id=session['id'])
-#         payment.status = 'completed'
-#         payment.save()
-
-#     return HttpResponse(status=200)
-
-# paypalrestsdk.configure({
-#     "mode": settings.PAYPAL_MODE,  # sandbox or live
-#     "client_id": settings.PAYPAL_CLIENT_ID,
-#     "client_secret": settings.PAYPAL_CLIENT_SECRET,
-# })
-
-# @login_required
-# def create_paypal_payment(request):
-#     if request.method == 'POST':
-#         orders = request.user.orders.filter(ordered=False)
-#         total_amount = sum(order.artwork.price * order.quantity for order in orders)
-
-#         payment = paypalrestsdk.Payment({
-#             "intent": "sale",
-#             "payer": {
-#                 "payment_method": "paypal"
-#             },
-#              "redirect_urls": {
-#                 "return_url": "http://localhost:8000/payments/success/",
-#                 "cancel_url": "http://localhost:8000/payments/cancel/"
-#             },
-#             "transactions": [{
-#                 "item_list": {
-#                     "items": [{
-#                         "name": order.artwork.title,
-#                         "sku": order.artwork.slug,
-#                         "price": str(order.artwork.price),
-#                         "currency": "USD",
-#                         "quantity": order.quantity
-#                     } for order in orders]
-#                 },
-#                 "amount": {
-#                     "total": str(total_amount),
-#                     "currency": "USD"
-#                 },
-#                 "description": "This is the payment transaction description."
-#             }]
-#         })
-
-#         if payment.create():
-#             for link in payment.links:
-#                 if link.rel == "approval_url":
-#                     approval_url = str(link.href)
-#                     return redirect(approval_url)
-#         else:
-#             return JsonResponse({'error': payment.error}, status=400)
-
-# @login_required
-# def execute_paypal_payment(request):
-#     payment_id = request.GET.get('paymentId')
-#     payer_id = request.GET.get('PayerID')
-#     payment = paypalrestsdk.Payment.find(payment_id)
-
-#     if payment.execute({"payer_id": payer_id}):
-#         # Enregistrer les détails du paiement dans la base de données
-#         orders = request.user.orders.filter(ordered=False)
-#         checkout = CheckOut.objects.create(customer=request.user)
-#         for order in orders:
-#             checkout.orders.add(order)
-
-#         Payment.objects.create(
-#             user=request.user,
-#             stripe_checkout_id=payment.id,
-#             amount=payment.transactions[0].amount.total,
-#             currency=payment.transactions[0].amount.currency,
-#             status='completed',
-#             checkout=checkout
-#         )
-
-#         for order in orders:
-#             order.ordered = True
-#             order.ordered_at = timezone.now()
-#             order.save()
-
-#         return redirect('payments/success/')
-#     else:
-#         return JsonResponse({'error': payment.error}, status=400)
-    
-
-
-# @login_required
-# def payment_success(request):
-#     return render(request, 'store/payments/success.html')
-
-# @login_required
-# def payment_cancel(request):
-#     return render(request, 'store/payments/cancel.html')
-
+# Code des paiements (Stripe, PayPal, etc.) peut être réactivé et ajusté si nécessaire
