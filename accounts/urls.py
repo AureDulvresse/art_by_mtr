d710308528eefecd940295a4f9dab62a6a80893e
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-07-25 16:54:59 +0100

    password reset updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path("update-account", views.update, name="update-account"),
     path('change-password/', views.change_password, name='change-password'),
     path('password_reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/pages/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     path("delete-account", views.destroy, name="delete-account"),
